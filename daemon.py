7e9602802f5fb23f4d5ab4b5cc3351a9256bca7d
Paul Chiang <paul_chiang@tcloudcomputing.com>
2010-07-27 17:19:53 +0800

    add Daemon.get_active_mon_ip_from_config

--- daemon.py
+++ daemon.py
@@ -1,8 +1,11 @@
+import ConfigParser
+
 class Daemon:
     service_globals = None
     var = None
     DAEMON_NAME = None
     conf_file_path = None
+    config = None
     INIT_SCRIPT = '/etc/init.d/ceph'
     TYPE = None
     def __init__(self, service_globals, varstore):
@@ -21,23 +24,44 @@ class Daemon:
     def get_ceph_id(self):
         return self.var.get_daemon_ceph_id(self)
 
-    def add_to_config(self, config): pass
+    def add_to_config(self, config): assert(0)
 
-    def prepare(self):
+    def set_config(self, config):
+        self.conf_file_path = config.get('tcloud', 'depot')
+        self.config = config
+
+    def setup(self):
         pass
+
     def activate(self):
         cmd = "%s -c %s --hostname %s start %s" % (self.INIT_SCRIPT, self.conf_file_path, self.get_host_ip(), self.TYPE)
         self.service_globals.run_remote_command(self.get_host_ip(), cmd)
 
     def status(self):
         pass
+
     def deactivate(self):
+        cmd = "%s -c %s --hostname %s stop %s" % (self.INIT_SCRIPT, self.conf_file_path, self.get_host_ip(), self.TYPE)
+        self.service_globals.run_remote_command(self.get_host_ip(), cmd)
+
+    def clean(self):
         pass
+
     def getDaemonArgs(self):
         return '-c %s' % self.conf_file_path
 
-    def set_config(self, config):
-        self.conf_file_path = config.get('tcloud', 'depot')
+    @staticmethod
+    def get_active_mon_ip_from_config(config):
+        active_mon_ip = None
+        i = 0
+        while active_mon_ip is None:
+            try:
+                addr_string = config.get('mon.%s' % i, 'addr')
+            except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+                i = i + 1
+            else:
+                (active_mon_ip, _, _) = addr_string.partition(':')
+        return (active_mon_ip, '%s' % i)
 
 
 
@@ -49,6 +73,12 @@ class Osd(Daemon):
 
     def add_to_config(self, config):
         config.add_mon(self.get_ceph_id(), self.get_host_ip())
+
+    def setup(self):
+        cmd = "mkdir -p %s" % (os.path.dirname(self.config.get('osd', 'osd data')),)
+        self.service_globals.run_remote_command(self.get_host_ip(), cmd)
+        cmd = "mkdir -p %s" % (os.path.dirname(self.config.get('osd', 'osd journal')),)
+        self.service_globals.run_remote_command(self.get_host_ip(), cmd)
 
 
 class Mds(Daemon):
@@ -67,21 +97,23 @@ class Mon(Daemon):
     def add_to_config(self, config):
         config.add_osd(self.get_ceph_id(), self.get_host_ip())
 
-    def prepare(self):
+    def setup(self):
         # add monitor to the mon map
         cmd = 'ceph -c %s mon add %s %s:6789' % (self.conf_file_path, self.get_host_id(), self.get_host_ip())
-        Depot.service_globals.run_shell_command(cmd)
+        self.service_globals.run_shell_command(cmd)
+
+        cmd = 'mkdir -p %s' % (os.path.dirname(self.config.get('mon', 'mon data')),)
 
         # copy mon dir from an existing to the new monitor
-        cmd = 'scp -r %s:%s/mon%d /tmp/mon' %  \
-                (args['monitorIP'], depot_conf_constants['MON_DATA_PREFIX'], args['monitorID'])
-        Depot.service_globals.run_shell_command(cmd)
+        (active_mon_ip, active_mon_id) = self.get_active_mon_ip_from_config(self.config)
+        cmd = 'scp -r %s:%s/mon%d %s:%s' %  \
+                (active_mon_ip, os.path.dirname(self.config.get('mon', 'mon data')), active_mon_id,
+                self.get_host_ip(), os.path.dirname(self.config.get('mon', 'mon data'))
+                )
+        self.service_globals.run_shell_command(cmd)
 
-        cmd = 'scp -r /tmp/mon %s:/tmp/mon%d' %  \
-                (info['IP'], info['cephid'])
-        Depot.service_globals.run_shell_command(cmd)
+    def deactivate(self):
+        cmd = 'ceph -c %s mon remove %s' % (self.conf_file_path, self.get_ceph_id(self))
+        self.service_globals.run_shell_command(cmd)
 
-        cmd = 'cp -r /tmp/mon%d %s' % (info['cephid'], depot_conf_constants['MON_DATA_PREFIX'])
-        self.service_globals.run_remote_command(info['IP'], cmd)
 
-        _DeployConfigrationFile(info['DepotID'])

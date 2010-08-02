54f8058b5a8ef08a9200c62d8deb9efd1a8b34d4
Paul Chiang <paul_chiang@tcloudcomputing.com>
2010-08-02 13:44:16 +0800

    test_tcdbvarstore: setup/teardown its own service_group

--- test_tcdbvarstore.py
+++ test_tcdbvarstore.py
@@ -11,7 +11,14 @@ from serviceglobals import TcdbResolv as Resolv
 
 class TestTcdbVarStore(TestVarStore):
     def setUp(self):
-        self.depot_uuid = 'd2c5d946-b888-40b3-aac2-adda05477a81'
+        self.depot_uuid = str(uuid.uuid4())
+        exec 'from serviceglobals import Tcdb'
+        conn = Tcdb.connect
+        cur = conn.cursor()
+        cur.execute('insert into "SERVICE_GROUP" values (%s, 4, 1)', (self.depot_uuid,))
+        conn.commit()
+        cur.close
+        conn.close()
         try:
             self.var = TcdbVarStore()
         except Exception as e:
@@ -20,6 +27,15 @@ class TestTcdbVarStore(TestVarStore):
 
     def run(self, *args, **kwds):
         unittest.TestCase.run(self, *args, **kwds)
+
+    def tearDown(self):
+        exec 'from serviceglobals import Tcdb'
+        conn = Tcdb.connect
+        cur = conn.cursor()
+        cur.execute('delete from "SERVICE_GROUP" where "ID"=%s', (self.depot_uuid,))
+        conn.commit()
+        cur.close
+        conn.close()
 
 if __name__ == '__main__':
     unittest.main()

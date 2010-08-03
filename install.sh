9f994d922d9e26b7d8739c8b025851152256b6d0
Paul Chiang <paul_chiang@tcloudcomputing.com>
2010-08-03 03:37:50 +0000

    install script & processagent cfg

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ds_lib=/opt/tcloud/Elaster/lib/python2.6/site-packages/tcloud/ds/
+
+install service.py $ds_lib
+install depot.py $ds_lib
+install daemon.py $ds_lib
+install serviceglobals.py $ds_lib
+install tccephconf.py $ds_lib
+install varstore.py $ds_lib
+install -t $ds_lib/ceph ceph/*.py
+install -b tcds.cfg /opt/tcloud/Elaster/etc/processagent/modules/tcds.cfg
